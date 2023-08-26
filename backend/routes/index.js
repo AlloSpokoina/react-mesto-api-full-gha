44a39db86cdc59eac337faa455ba7e7d25e797f2
Valeriya Markina <118906987+AlloSpokoina@users.noreply.github.com>
2023-08-27 01:23:55 +0500

    add test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,12 @@ const signupRouter = require('./signup');
 const signinRouter = require('./signin');
 const auth = require('../middlewares/auth');
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.use('/signup', signupRouter);
 router.use('/signin', signinRouter);
 router.use(auth);
