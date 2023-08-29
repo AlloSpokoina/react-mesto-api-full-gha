0178d4a5b5d9231d10899f3567755e3dd21aac33
Valeriya Markina <118906987+AlloSpokoina@users.noreply.github.com>
2023-08-29 19:02:00 +0500

    fix test

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -20,13 +20,13 @@ router.post('/', celebrate({
   }),
 }), addCard);
 
-router.put('/cards/likes/', celebrate({
+router.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex().required(),
   }),
 }), likeCard);
 
-router.delete('/cards/likes/', celebrate({
+router.delete('/:cardId/likes', celebrate({
   params: Joi.object().keys({
     cardId: Joi.string().length(24).hex().required(),
   }),
